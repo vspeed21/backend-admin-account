1e1a1d4c1c1336f00f65948410fdf5c9162cb015
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-11-11 13:45:09 -0600

    index: routes for admin added

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 import connectDB from './config/db.js';
+import AdminRoutes from './routes/AdminRoutes.js';
 
 const app = express();
 
 dotenv.config();
 connectDB();
+
+app.use('/api/admin', AdminRoutes);
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
