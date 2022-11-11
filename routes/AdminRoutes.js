500f9b01051cfe15f4c1915cdf5b784ec1b51106
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-11-11 15:01:55 -0600

    routes: checkToken & savePassword function added

--- routes/AdminRoutes.js
+++ routes/AdminRoutes.js
@@ -4,12 +4,16 @@ import {
   signUp,
   confirmAcc,
   forgotPasswordSendEmail,
+  checkToken,
+  savePassword
 }from '../controllers/AdminControllers.js'
 
 const router = express.Router();
 
+//Public area
 router.post('/', signUp);
 router.get('/confirm/:token', confirmAcc);
-router.post('/forgot-password', forgotPasswordSendEmail)
+router.post('/forgot-password', forgotPasswordSendEmail);
+router.route('/forgot-password/:token').get(checkToken).post(savePassword);
 
 export default router;
