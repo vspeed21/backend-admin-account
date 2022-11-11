300a74ad32f3daf12324504479f31a986afa707f
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-11-11 14:53:45 -0600

    routes: forgotPasswordSendEmail function added

--- routes/AdminRoutes.js
+++ routes/AdminRoutes.js
@@ -3,11 +3,13 @@ import express from 'express';
 import {
   signUp,
   confirmAcc,
+  forgotPasswordSendEmail,
 }from '../controllers/AdminControllers.js'
 
 const router = express.Router();
 
 router.post('/', signUp);
 router.get('/confirm/:token', confirmAcc);
+router.post('/forgot-password', forgotPasswordSendEmail)
 
 export default router;
