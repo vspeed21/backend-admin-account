e2bf4fb0e8363fad83c484c1d311a87f005a069c
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-11-11 13:29:57 -0600

    config to env files with dotenv

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+import connectDB from './config/db.js';
 
 const app = express();
+
+dotenv.config();
+connectDB();
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
