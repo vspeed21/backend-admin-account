f5085ed5044e961639c410bc170dbcc8553e512b
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-11-12 14:33:56 -0600

    controllers: addAccount done

--- controllers/AccountControllers.js
+++ controllers/AccountControllers.js
@@ -1,3 +1,13 @@
+import Account from "../models/Account.js";
+
 export const addAccount = async (req, res) => {
-  
+  const account = new Account(req.body);
+  account.admin = req.admin._id;
+
+  try {
+    await account.save();
+    res.json({msg: 'Cuenta agregada correctamente'});
+  } catch (error) {
+    console.log(error);
+  }
 }
