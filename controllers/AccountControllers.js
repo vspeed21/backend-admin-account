efd7e3c3e2027cbe89ac82edffc911567f63b81e
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-11-12 22:25:11 -0600

    updateAcc function done

--- controllers/AccountControllers.js
+++ controllers/AccountControllers.js
@@ -16,3 +16,34 @@ export const getAccounts = async (req, res) => {
   const accounts = await Account.find().where('admin').equals(req.admin);
   res.json(accounts);
 }
+
+export const updateAcc = async (req, res) => {
+  const { id } = req.params;
+  const { name, screen, pin, card, deadline} = req.body;
+
+  const account = await Account.findById(id);
+
+  if(!account) {
+    return res.status(404).json({msg: "NOT FOUND"});
+  }
+
+  if(account.admin._id.toString() !== req.admin._id.toString()) {
+    return res.json({msg: "Accion no valida"})
+  }
+  
+  try {
+    account.name = name || account.name;
+    account.screen = screen || account.screen;
+    account.pin = pin || account.pin;
+    account.card = card || account.card;
+    account.deadline = deadline || account.deadline;
+
+    await account.save();
+
+    res.json({msg: "Modificado correctamente"});
+
+  } catch (error) {
+    console.log(error);
+  }
+  
+}
