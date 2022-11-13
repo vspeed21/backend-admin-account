0064fce78f8ca34a31914855492cc6d011a2dfa5
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-11-12 22:12:42 -0600

    routes: last routes for account

--- routes/AccountRoutes.js
+++ routes/AccountRoutes.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import { 
   addAccount, 
+  destroyAcc, 
   getAccounts,
+  updateAcc,
 } from '../controllers/AccountControllers.js';
 import checkAuth from '../middlewares/checkAuth.js';
 
 const router = express.Router();
 
 router.route('/').post(checkAuth, addAccount).get(checkAuth, getAccounts);
+
+router.route('/:id')
+      .put(checkAuth, updateAcc)
+      .delete(checkAuth, destroyAcc);
 
 export default router;
